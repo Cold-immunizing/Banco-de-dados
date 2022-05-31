aa7a11b1197becd2d74285783733da785922f2e5
Matheus Gregorin <matheus.gregorin@sptech.school>
2022-05-31 14:16:14 -0300

    Banco atualizado

--- BDsprint2/ScriptBancoDeDadosSprint3.sql
+++ BDsprint2/ScriptBancoDeDadosSprint3.sql
@@ -1,7 +1,6 @@
 create database immuncold;
 use immuncold;
 
-
 create table empresa (
 id_empresa int not null primary key auto_increment,
 nome varchar(255) not null,
@@ -13,7 +12,10 @@ telefone varchar(14) not null unique,
 email varchar(100) not null unique,
 senha varchar(25)not null) auto_increment = 1000;
 
+
+#inserir primeiros dados no banco tabela Empresa
 insert into empresa values (null, 'teste1', 'quaquer1', '1231231123', '1231231231', 'Rua sakjdhaskda', '1193232423', 'teste1@gmail.com', '1234');
+
 
 create table usuario (
 id_usuario int not null primary key auto_increment,
@@ -32,9 +34,11 @@ check (tipo = 'Transporte' or tipo = 'Armazenamento'),
 endereco_localidade varchar(150) not null,
 identificador varchar(25) not null);
 
-insert into localidade values (null, 'Armazenamento', 'ASDJKASDHASDKASDA', '1A');
 
+#inserir primeiros dados no banco tabela localidade
+insert into localidade values (null, 'Armazenamento', 'ASDJKASDHASDKASDA', '1A');
 insert into localidade values (null, 'Armazenamento', 'askjdasdhajk', 'A');
+
 
 create table parametro (
 id_parametro int not null primary key auto_increment,
@@ -53,14 +57,10 @@ references localidade(id_localidade),
 fk_parametro int,
 foreign key (fk_parametro) references parametro(id_parametro));
 
-select * from empresa;
+
+#inserir primeiros dados no banco tabela sensor
 insert into sensor (fk_empresa, fk_localidade) values ('1000', '1');
-
-select * from empresa;
-
-insert into sensor (nome_vacina, fk_empresa, fk_localidade) values ('Pfizer', '1001', '1');
-
-select * from sensor;
+insert into sensor (nome_vacina, fk_empresa, fk_localidade) values ('Pfizer', '1000', '2');
 
 
 
@@ -72,39 +72,34 @@ fk_sensor int not null,
 foreign key (fk_sensor)
 references sensor(id_sensor));
 
+#inserir primeiros dados no banco tabela hist_medicao
+insert into hist_medicao (lm35, fk_sensor) values ('30', 1),
+('29', 1),
+('20', 1),
+('15', 1),
+('30', 1),
+('23', 1),
+('34', 1),
+('25', 1),
+('10', 1),
+('50', 1),
+('40', 1);
 
-insert into hist_medicao (lm35, fk_sensor) values ('30', 4),
-('29', 4),
-('20', 4),
-('15', 4),
-('30', 4),
-('23', 4),
-('34', 4),
-('25', 4),
-('10', 4),
-('50', 4),
-('40', 4);
 
-insert into hist_medicao (lm35, fk_sensor) values ('30', 4),
-('29', 4),
-('20', 4),
-('15', 4),
-('30', 4),
-('23', 4),
-('34', 4),
-('25', 4),
-('50', 4),
-('100', 4),
-('40', 4);
+# inserir dados com diferentes ID de Sensores
+insert into hist_medicao (lm35, fk_sensor) values ('70', 3);
+insert into hist_medicao (lm35, fk_sensor) values ('1', 5);
+insert into hist_medicao (lm35, fk_sensor) values ('20', 6);
+insert into hist_medicao (lm35, fk_sensor) values ('5', 1);
 
-insert into hist_medicao (lm35, fk_sensor) values ('16', 4);
-
+# Select Que faz a busca no Banco
 select * from hist_medicao order by data_horario desc limit 1;
 
 
-
+# select's
+select * from usuario;
+select * from parametros;
+select * from localidade;
 select * from hist_medicao;
-
 select * from sensor;
-
 select * from empresa;
